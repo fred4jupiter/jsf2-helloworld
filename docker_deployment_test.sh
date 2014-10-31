0e1fe4ab537ccfd1d105b8e81aea43f55d88b2b3
lubuntu <hamsterhase@gmx.de>
2014-10-31 18:53:42 +0100

    changed for sudo

--- docker_deployment_test.sh
+++ docker_deployment_test.sh
@@ -3,9 +3,11 @@
 # running your local registry
 # sudo docker run -p 5000:5000 -d registry
 
-sudo docker build -t mstaehler/jsf2-helloworld .
-sudo docker stop hello_test
-sudo docker rm hello_test
-sudo docker tag mstaehler/jsf2-helloworld localhost:5000/mstaehler/jsf2-helloworld
-sudo docker push localhost:5000/mstaehler/jsf2-helloworld
-sudo docker run -d --name hello_test -p 49160:8080 -e spring.profiles.active=test -t localhost:5000/mstaehler/jsf2-helloworld 
+# NOTE: Run this script as sudo: sudo docker_deployment_test.sh
+
+docker build -t mstaehler/jsf2-helloworld .
+docker stop hello_test
+docker rm hello_test
+docker tag mstaehler/jsf2-helloworld localhost:5000/mstaehler/jsf2-helloworld
+docker push localhost:5000/mstaehler/jsf2-helloworld
+docker run -d --name hello_test -p 49160:8080 -e spring.profiles.active=test -t localhost:5000/mstaehler/jsf2-helloworld 
